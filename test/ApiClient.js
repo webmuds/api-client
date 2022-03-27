3e4b8de2923810ecf68eee6f167f9cd205207910
pauldps <github@pauloddr.com>
2022-03-27 14:45:08 -0400

    nock + minimal specs

--- test/ApiClient.js
+++ test/ApiClient.js
@@ -1,9 +1,12 @@
 'use strict'
 
 import { expect } from '@dimensionalpocket/development'
+import Nock from 'nock'
 
 import ApiClient from '../index.js'
 import { ApiClient as ApiClientFromSrc } from '../src/ApiClient.js'
+
+const BASE_URL = 'http://example.test'
 
 describe('ApiClient', function () {
   it('loads ApiClient from /src', function () {
@@ -12,12 +15,28 @@ describe('ApiClient', function () {
 
   describe('#setHeader', function () {
     before(function () {
-      this.client = new ApiClient('http://example.test')
+      this.client = new ApiClient(BASE_URL)
       this.client.setHeader('test-Header', 'testValue')
     })
 
     it('sets a header', function () {
       expect(this.client.instance.defaults.headers.common['test-Header']).to.eq('testValue')
+    })
+  })
+
+  describe('#get', function () {
+    before(async function () {
+      this.scope = Nock(BASE_URL).get('/things').reply(200, { success: true })
+      this.api = new ApiClient(BASE_URL)
+      this.body = await this.api.get('/things')
+    })
+
+    after(function () {
+      this.scope.done()
+    })
+
+    it('returns the correct body', function () {
+      expect(this.body.success).to.eq(true)
     })
   })
 })
